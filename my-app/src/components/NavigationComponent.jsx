000f4e9afb2f2c03151edaa73dcbb2c41b6d2482
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-08-12 15:19:54 -0500

    [feat]navigation

--- my-app/src/components/NavigationComponent.jsx
+++ my-app/src/components/NavigationComponent.jsx
@@ -1,9 +1,21 @@
+import Nav from 'react-bootstrap/Nav';
+
 export const NavigationComponent = () => {
   return (
-    <nav>
-      <a href="/">Inicio</a>
-      <a href="/mi-album">Mi álbum</a>
-      <a href="/obtener-laminas">Obtener Láminas</a>
-    </nav>
+    <Nav className="justify-content-center my-5" activeKey="/">
+      <Nav.Item>
+        <Nav.Link href="/">Inicio</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="mi-album" href="/mi-album">
+          Mi Álbum
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="obtener-laminas" href="/obtener-laminas">
+          Obtener Láminas
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 };
