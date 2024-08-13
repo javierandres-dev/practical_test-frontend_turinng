ecdffb9744a0e4fb614dfa9d7335d7f34265b6e2
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-08-12 21:39:09 -0500

    [fix]reload pages

--- my-app/src/components/NavigationComponent.jsx
+++ my-app/src/components/NavigationComponent.jsx
@@ -1,18 +1,21 @@
+import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 
 export const NavigationComponent = () => {
   return (
     <Nav className="justify-content-center my-5" activeKey="/">
       <Nav.Item>
-        <Nav.Link href="/">Inicio</Nav.Link>
+        <Nav.Link as={Link} to="/">
+          Inicio
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="mi-album" href="/mi-album">
+        <Nav.Link as={Link} eventKey="mi-album" to="/mi-album">
           Mi Álbum
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="obtener-laminas" href="/obtener-laminas">
+        <Nav.Link as={Link} eventKey="obtener-laminas" to="/obtener-laminas">
           Obtener Láminas
         </Nav.Link>
       </Nav.Item>
