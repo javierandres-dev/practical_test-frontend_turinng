dd3316bde35342bc7d5350fa8543b58dd74db831
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-08-14 14:59:37 -0500

    [feat]album

--- my-app/src/components/AlbumComponent.jsx
+++ my-app/src/components/AlbumComponent.jsx
@@ -1,40 +1,94 @@
+import { useContext, useEffect, useState } from 'react';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { GlobalContext } from '../contexts/GlobalContext';
 
 export const AlbumComponent = () => {
-  const sections = [
-    {
-      header: 'Películas',
-      title: '🎥',
-      text: '6 láminas posibles',
-      btn: 'Ver Álbum',
-      footer: 'x láminas obtenidas',
-    },
-    {
-      header: 'Personajes',
-      title: '⭐',
-      text: '82 láminas posibles',
-      btn: 'Ver Álbum',
-      footer: 'x láminas obtenidas',
-    },
-    {
-      header: 'Naves',
-      title: '☄️',
-      text: '36 láminas posibles',
-      btn: 'Ver Álbum',
-      footer: 'x láminas obtenidas',
-    },
-  ];
+  const { gAlb } = useContext(GlobalContext);
+  const [album, setAlbum] = gAlb;
+  const [sections, setSections] = useState([]);
 
-  const cards = sections.map((section) => (
+  useEffect(() => {
+    let nMovies = 0,
+      nCharacters = 0,
+      nNaves = 0;
+    const arr = album.sort((a, b) => a.sheetNumber - b.sheetNumber);
+    for (const item of arr) {
+      if (item.albumSection === 'Películas') nMovies++;
+      if (item.albumSection === 'Personajes') nCharacters++;
+      if (item.albumSection === 'Naves') nNaves++;
+    }
+    setAlbum(arr);
+    setSections([
+      {
+        header: 'Películas', // 🎥
+        footer: `${nMovies} láminas obtenidas de 6 láminas posibles`,
+      },
+      {
+        header: 'Personajes', // ⭐
+        footer: `${nCharacters} láminas obtenidas de 82 láminas posibles`,
+      },
+      {
+        header: 'Naves', // ☄️
+        footer: `${nNaves} láminas obtenidas de 36 láminas posibles`,
+      },
+    ]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const sheets = sections.map((section) => (
     <CardGroup key={section.header}>
-      <Card className="text-center">
+      <Card
+        border={
+          section.header === 'Películas'
+            ? 'primary'
+            : section.header === 'Personajes'
+            ? 'info'
+            : 'dark'
+        }
+        className="text-center my-3"
+      >
         <Card.Header>{section.header}</Card.Header>
         <Card.Body>
-          <Card.Title>{section.title}</Card.Title>
-          <Card.Text>{section.text}</Card.Text>
-          <Button variant="primary">{section.btn}</Button>
+          <div className="d-flex gap-2 flex-wrap">
+            {album.map(
+              (sheet) =>
+                section.header === sheet.albumSection && (
+                  <div
+                    key={sheet.sheetId}
+                    className={
+                      sheet.albumSection === 'Películas'
+                        ? 'border border-primary border-opacity-50 rounded rounded-3'
+                        : sheet.albumSection === 'Personajes'
+                        ? 'border border-info border-opacity-50 rounded rounded-3'
+                        : 'border border-dark border-opacity-50 rounded rounded-3'
+                    }
+                    style={{
+                      width: '300px',
+                    }}
+                  >
+                    <ListGroup>
+                      <ListGroup.Item>
+                        # {sheet.sheetNumber} Lámina{' '}
+                        <span
+                          className={
+                            sheet.categoryGroup === 'Especial'
+                              ? 'text-primary'
+                              : 'text-secondary'
+                          }
+                        >
+                          {sheet.categoryGroup}
+                        </span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        {sheet.name || sheet.title}
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </div>
+                )
+            )}
+          </div>
         </Card.Body>
         <Card.Footer className="text-muted">{section.footer}</Card.Footer>
       </Card>
@@ -44,7 +98,7 @@ export const AlbumComponent = () => {
   return (
     <>
       <h1>Mi Álbum</h1>
-      {cards}
+      {sheets}
     </>
   );
 };
