808f3db6df64f04d5118d7c0d9ce59d737afcc60
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-08-14 15:20:28 -0500

    [feat]pages

--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 export const HomePage = () => {
   return (
-    <h1 className="display-6">
+    <h1 className="display-6 text-center">
       Colección de láminas digitales basados en universo Star Wars.
     </h1>
   );
